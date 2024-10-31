b2c886292bb3b8d219c01ee599fd321fb465e0b0
Brent Beamer <Beamerbrent38@gmail.com>
2024-10-31 05:04:17 -0500

    first commit

---
+++ MidMoWeather/MidMoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidMoWeatherApp.swift
+//  MidMoWeather
+//
+//  Created by Brent Beamer on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MidMoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MidMoWeatherTests/MidMoWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MidMoWeatherTests.swift
+//  MidMoWeatherTests
+//
+//  Created by Brent Beamer on 10/30/24.
+//
+
+import Testing
+@testable import MidMoWeather
+
+struct MidMoWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
